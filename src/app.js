b87d5e64bf1680f20b445d435761b0658bdb0ba2
horngyih <horngyih@yahoo.com>
2017-11-22 06:03:48 +0800

    - Added start script target for heroku deployment - Prepared app code for caching

--- src/app.js
+++ src/app.js
@@ -10,21 +10,35 @@ var cache = {};
 function processBinLookup( req, res ){
     var prefix = req.params.prefix;
     if( prefix ){
-        var result = cache[prefix];
+        var result = getFromCache(prefix);
         if( !result ){
             console.log( "Looking up ", prefix );
             _binlookup(prefix)
             .then(function(data){
+                putToCache( prefix, data );
                 res.status(200).send(data).end();
             })
             .catch(function(err){
                 console.log( err );
                 res.sendStatus(404);
             });
+        } else {
+            console.log( "Cached" );
+            res.status(200).send(result).end();
         }
     } else {
         res.sendStatus(200);
     }
+}
+
+function getFromCache(prefix){
+    cache = cache || {};
+    return cache[prefix];
+}
+
+function putToCache(prefix, data){
+    cache = cache || {};
+    cache[prefix] = data;
 }
 
 module.exports = server;
