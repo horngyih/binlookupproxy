9014f40f48010593e427c7fcf027464f0e7d07a4
horngyih <horngyih@yahoo.com>
2017-11-22 06:56:41 +0800

    - Prepared code for REDIS Cache

--- src/app.js
+++ src/app.js
@@ -1,44 +1,87 @@
 var _express = require("express");
 var _binlookup = require("binlookup")();
+var _redis = require("redis");
 
 var server = _express();
 
 server.get("/lookup/:prefix", processBinLookup );
 
-var cache = {};
+
+var cache = (process.env.REDIS_URL)?_redis.createClient(process.env.REDIS_URL):{};
+var cache_period = process.env.CACHE_TTL || 60*60; //Default 1 hour
 
 function processBinLookup( req, res ){
     var prefix = req.params.prefix;
     if( prefix ){
-        var result = getFromCache(prefix);
-        if( !result ){
+        lookupCache(prefix)
+        .then(function(data){
+            console.log( "Cached - ", data );
+            respond(res, data);
+        })
+        .catch(function(data){
             console.log( "Looking up ", prefix );
-            _binlookup(prefix)
-            .then(function(data){
-                putToCache( prefix, data );
-                res.status(200).send(data).end();
-            })
-            .catch(function(err){
-                console.log( err );
-                res.sendStatus(404);
-            });
-        } else {
-            console.log( "Cached" );
-            res.status(200).send(result).end();
-        }
+            directLookup(prefix,res);
+        });
     } else {
         res.sendStatus(200);
     }
 }
 
+function directLookup( prefix, res ){
+    _binlookup(prefix)
+    .then(function(data){
+        putToCache( prefix, data );
+        respond(res,data);
+    })
+    .catch(function(err){
+        console.log( err );
+        res.sendStatus(404);
+    });
+}
+
+function lookupCache(prefix){
+    return new Promise(function(resolve,reject){
+        getFromCache(prefix)
+        .then(resolve,reject);
+    });
+}
+
+function respond( res, data ){
+    res.status(200).send(data).end();
+}
+
 function getFromCache(prefix){
     cache = cache || {};
-    return cache[prefix];
+    return new Promise(function(resolve, reject){
+        if( typeof cache.hgetall === "function" ){
+            cache.hgetall(prefix,function(err,data){
+                if( err ){
+                    reject(err);
+                } else {
+                    resolve(data);
+                }
+            });
+        } else {
+            var cached = cache[prefix];
+            if( cached ){
+                resolve(cached);
+            } else {
+                reject();
+            }
+        }
+    });
 }
 
 function putToCache(prefix, data){
     cache = cache || {};
-    cache[prefix] = data;
+    if( typeof cache.hmset === "function" ){
+        cache.hmset(prefix, data);
+        if( cache_period ){
+            cache.expire(prefix, cache_period);
+        }
+    } else {
+        cache[prefix] = data;
+    }
 }
 
 module.exports = server;
